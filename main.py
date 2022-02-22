021e520c9e402c8fee94817081505dfba7d2215c
Fox5113 <demon5113@list.ru>
2022-02-22 20:59:46 +0300

    Add class for laptop...

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
+import json
+
+class Laptop:
+    def __init__(self, name, href, model):
+        self.name = name
+        self.href = href
+        self.model = model
+
+
+class Models:
+    def __init__(self, models):
+        self.allmodel = models
+
+    def __str__(self):
+        return "name : " + self.name + " href : " + self.href
 
 
 def filter_empty_item(web_element):
@@ -16,8 +31,20 @@ if __name__ == '__main__':
     driver.maximize_window()
     thinkpad = driver.find_element(By.XPATH, "//img[@alt='THINKPAD: ']")
     thinkpad.click()
-    allmodels_name = (driver.find_elements(By.XPATH, "//div[@class='model-title']/a"))
-    allmodels_name = list(filter(filter_empty_item, allmodels_name))
+    allmodels_name = list(filter(filter_empty_item, (driver.find_elements(By.XPATH, "//div[@class='model-title']/a"))))
+
     for model in allmodels_name:
+
         print(model.text)
+    all_laptop = Models(list(filter(filter_empty_item,
+                                    driver.find_elements(By.XPATH, "//a[@class='vam-subseries']"))))
+    laptops = list()
+    for model in all_laptop:
+        item = Laptop(model.text, model.get_dom_attribute("href"))
+        laptops.append(item)
+
+    for item in laptops:
+        print(item)
+
+    result = json.dumps(laptops)
     driver.close()
