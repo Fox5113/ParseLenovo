64376417f74255f626d02164e61650bf2b5fefed
Fox5113 <demon5113@list.ru>
2022-02-22 22:24:32 +0300

    Reformat code

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 import json
 
+
 class Laptop:
     def __init__(self, name, href, model):
         self.name = name
@@ -29,22 +30,26 @@ if __name__ == '__main__':
     # driver = webdriver.Firefox(options=firefox_options)
     driver.get("https://www.lenovo.com/by/ru/laptops/c/Laptops#view-all")
     driver.maximize_window()
-    thinkpad = driver.find_element(By.XPATH, "//img[@alt='THINKPAD: ']")
-    thinkpad.click()
-    allmodels_name = list(filter(filter_empty_item, (driver.find_elements(By.XPATH, "//div[@class='model-title']/a"))))
-
-    for model in allmodels_name:
-
-        print(model.text)
-    all_laptop = Models(list(filter(filter_empty_item,
-                                    driver.find_elements(By.XPATH, "//a[@class='vam-subseries']"))))
+    tags = ['THINKPAD', 'ThinkBook Laptops: ', 'IDEAPAD: ', 'LENOVO: ', 'YOGA: ',
+            'Игровые ноутбуки Lenovo Legion: Lenovo Legion Logo']
+    all_laptop = list()
     laptops = list()
-    for model in all_laptop:
-        item = Laptop(model.text, model.get_dom_attribute("href"))
-        laptops.append(item)
+    for tag in tags:
+        category = driver.find_element(By.XPATH, f"//img[@alt='{tag}']")
+        category.click()
+        models_in_category = list(filter(filter_empty_item, (driver.find_elements(By.XPATH,
+                                                                                  "//div[@class='model-title']/a"))))
+        for model in models_in_category:
+            print(model.text)
+        all_laptop.extend(Models(list(filter(filter_empty_item,
+                                             driver.find_elements(By.XPATH, "//a[@class='vam-subseries']")))))
 
-    for item in laptops:
-        print(item)
+        for model in all_laptop:
+            item = Laptop(model.text, model.get_dom_attribute("href"))
+            laptops.append(item)
+
+        for item in laptops:
+            print(item)
 
     result = json.dumps(laptops)
     driver.close()
