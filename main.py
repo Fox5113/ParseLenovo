5d9153a8dc3e1002124a4410348ca6e8b2e462d4
Fox5113 <demon5113@list.ru>
2022-02-23 16:38:21 +0300

    Added feature parsing.

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
+import json
+from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
-import json
 
 
 class Laptop:
-    def __init__(self, name, href, model):
+    def __init__(self, name, href):
         self.name = name
         self.href = href
-        self.model = model
+        self.features = dict()
 
 
 class Models:
     def __init__(self, models):
-        self.allmodel = models
+        self.models = models
 
     def __str__(self):
         return "name : " + self.name + " href : " + self.href
@@ -23,15 +24,18 @@ def filter_empty_item(web_element):
     return len(web_element.text) > 0
 
 
+def get_laptop_features(driver, laptop):
+    driver.get(r"https://www.lenovo.com" + laptop.href)
+    item = list(filter(filter_empty_item, driver.find_elements(By.XPATH, "//tbody/tr/td")))
+    return item
+
+
 if __name__ == '__main__':
     driver = webdriver.Firefox()
-    # firefox_options = Options()
-    # firefox_options.set_headless()
-    # driver = webdriver.Firefox(options=firefox_options)
     driver.get("https://www.lenovo.com/by/ru/laptops/c/Laptops#view-all")
     driver.maximize_window()
-    tags = ['THINKPAD', 'ThinkBook Laptops: ', 'IDEAPAD: ', 'LENOVO: ', 'YOGA: ',
-            'Игровые ноутбуки Lenovo Legion: Lenovo Legion Logo']
+    tags = ["THINKPAD: ", "ThinkBook Laptops: ", "IDEAPAD: ", "LENOVO: ", "YOGA: ",
+            "Игровые ноутбуки Lenovo Legion: Lenovo Legion Logo"]
     all_laptop = list()
     laptops = list()
     for tag in tags:
@@ -41,15 +45,25 @@ if __name__ == '__main__':
                                                                                   "//div[@class='model-title']/a"))))
         for model in models_in_category:
             print(model.text)
-        all_laptop.extend(Models(list(filter(filter_empty_item,
-                                             driver.find_elements(By.XPATH, "//a[@class='vam-subseries']")))))
+        laptop_from_page = Models(list(filter(filter_empty_item,
+                                              driver.find_elements(By.XPATH, "//a[@class='vam-subseries']"))))
+        all_laptop.extend(laptop_from_page.models)
 
         for model in all_laptop:
             item = Laptop(model.text, model.get_dom_attribute("href"))
             laptops.append(item)
 
-        for item in laptops:
-            print(item)
-
-    result = json.dumps(laptops)
+    for laptop in laptops:
+        features = list()
+        while True:
+            try:
+                features = get_laptop_features(driver, laptop)
+                break
+            except Exception:
+                sleep(5)
+        driver.delete_all_cookies()
+        for i in range(0, len(features) - 2, 2):
+            laptop.features[features[i].text] = features[i + 1].text
     driver.close()
+    with open('lenovo_product.json', 'w') as f:
+        json.dump(laptops, f)
