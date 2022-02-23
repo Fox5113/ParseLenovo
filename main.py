4adbcd316e5f3dbab9585538107b94381fbabf02
Fox5113 <demon5113@list.ru>
2022-02-23 20:36:07 +0300

    Project final commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import pickle
 from time import sleep
 
 from selenium import webdriver
@@ -11,13 +12,13 @@ class Laptop:
         self.href = href
         self.features = dict()
 
+    def __str__(self):
+        return "name : " + self.name + " href : " + self.href
+
 
 class Models:
     def __init__(self, models):
         self.models = models
-
-    def __str__(self):
-        return "name : " + self.name + " href : " + self.href
 
 
 def filter_empty_item(web_element):
@@ -28,6 +29,10 @@ def get_laptop_features(driver, laptop):
     driver.get(r"https://www.lenovo.com" + laptop.href)
     item = list(filter(filter_empty_item, driver.find_elements(By.XPATH, "//tbody/tr/td")))
     return item
+
+
+def obj_dict(obj):
+    return obj.__dict__
 
 
 if __name__ == '__main__':
@@ -43,16 +48,14 @@ if __name__ == '__main__':
         category.click()
         models_in_category = list(filter(filter_empty_item, (driver.find_elements(By.XPATH,
                                                                                   "//div[@class='model-title']/a"))))
-        for model in models_in_category:
-            print(model.text)
         laptop_from_page = Models(list(filter(filter_empty_item,
                                               driver.find_elements(By.XPATH, "//a[@class='vam-subseries']"))))
         all_laptop.extend(laptop_from_page.models)
 
         for model in all_laptop:
-            item = Laptop(model.text, model.get_dom_attribute("href"))
-            laptops.append(item)
-
+            if len(model.text) > 0:
+                item = Laptop(model.text, model.get_dom_attribute("href"))
+                laptops.append(item)
     for laptop in laptops:
         features = list()
         while True:
@@ -65,5 +68,6 @@ if __name__ == '__main__':
         for i in range(0, len(features) - 2, 2):
             laptop.features[features[i].text] = features[i + 1].text
     driver.close()
-    with open('lenovo_product.json', 'w') as f:
-        json.dump(laptops, f)
+    with open('data.json', 'w') as f:
+        json_string = json.dumps(laptops, default=obj_dict)
+        f.write(json_string)
